320aa77f82d17cdbe9c9d23ae2dbc3d863daab30
chengyi818 <chengyi818@foxmail.com>
2019-01-11 16:54:11 +0800

    Develop | Lab: Finish Lab4 Exercise3

--- jos/lab4/kern/pmap.c
+++ jos/lab4/kern/pmap.c
@@ -302,6 +302,9 @@ mem_init(void)
 // Modify mappings in kern_pgdir to support SMP
 //   - Map the per-CPU stacks in the region [KSTACKTOP-PTSIZE, KSTACKTOP)
 //
+// PTSIZE: 4 MB
+// KSTKSIZE: 32 KB
+// KSTKGAP: 32 KB
 static void
 mem_init_mp(void)
 {
@@ -321,6 +324,11 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
+	for(int i=0; i<NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE+KSTKGAP);
+		boot_map_region(kern_pgdir, kstacktop_i-KSTKSIZE, KSTKSIZE,
+				PADDR(percpu_kstacks[i]), PTE_W | PTE_P);
+	}
 
 }
 

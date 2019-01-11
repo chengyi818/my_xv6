a3caaa533cb307c77d2264bc5a0fbea719fbaf66
chengyi818 <chengyi818@foxmail.com>
2019-01-11 17:20:58 +0800

    Develop | Lab: Finish Lab4 Exercise4

--- jos/lab4/kern/trap.c
+++ jos/lab4/kern/trap.c
@@ -118,6 +118,7 @@ trap_init(void)
 }
 
 // Initialize and load the per-CPU TSS and IDT
+// 每个CPU都会单独运行这段代码
 void
 trap_init_percpu(void)
 {
@@ -148,21 +149,43 @@ trap_init_percpu(void)
 
 	// Setup a TSS so that we get the right stack
 	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
-	ts.ts_iomb = sizeof(struct Taskstate);
+	int cpu_id = cpunum();
+	uintptr_t kstacktop_i = KSTACKTOP - cpu_id * (KSTKSIZE+KSTKGAP);
+	thiscpu->cpu_ts.ts_esp0 = kstacktop_i;
+	thiscpu->cpu_ts.ts_ss0 = GD_KD;
+	thiscpu->cpu_ts.ts_iomb = sizeof(struct Taskstate);
 
 	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
-					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+	gdt[(GD_TSS0 >> 3) + cpu_id] = SEG16(STS_T32A, (uint32_t) (&(thiscpu->cpu_ts)),
+				    sizeof(struct Taskstate) - 1, 0);
+	gdt[(GD_TSS0 >> 3) + cpu_id].sd_s = 0;
 
 	// Load the TSS selector (like other segment selectors, the
 	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+	ltr(GD_TSS0+(cpu_id<<3));
 
 	// Load the IDT
 	lidt(&idt_pd);
+
+
+	// comment after lab4 Exercise4
+	/* // Setup a TSS so that we get the right stack */
+	/* // when we trap to the kernel. */
+	/* ts.ts_esp0 = KSTACKTOP; */
+	/* ts.ts_ss0 = GD_KD; */
+	/* ts.ts_iomb = sizeof(struct Taskstate); */
+
+	/* // Initialize the TSS slot of the gdt. */
+	/* gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts), */
+	/* 				sizeof(struct Taskstate) - 1, 0); */
+	/* gdt[GD_TSS0 >> 3].sd_s = 0; */
+
+	/* // Load the TSS selector (like other segment selectors, the */
+	/* // bottom three bits are special; we leave them 0) */
+	/* ltr(GD_TSS0); */
+
+	/* // Load the IDT */
+	/* lidt(&idt_pd); */
 }
 
 void

e5ffe0dcccdddbc021e7f2b9c0f6fe4fefc66e19
chengyi818 <chengyi818@foxmail.com>
2019-04-27 21:21:40 +0800

    Init lab6

---
+++ jos/lab6/user/forktree.c
@@ -0,0 +1,42 @@
+// Fork a binary tree of processes and display their structure.
+
+#include <inc/lib.h>
+
+/* #define __DEBUG__ */
+#include <inc/cydebug.h>
+
+#define DEPTH 3
+
+void forktree(const char *cur);
+
+void
+forkchild(const char *cur, char branch)
+{
+	char nxt[DEPTH+1];
+
+	if (strlen(cur) >= DEPTH)
+		return;
+
+	DEBUG("envid: %d ,cur: %s ,branch: %c\n", thisenv->env_id, cur, branch);
+
+	snprintf(nxt, DEPTH+1, "%s%c", cur, branch);
+	if (fork() == 0) {
+		forktree(nxt);
+		exit();
+	}
+}
+
+void
+forktree(const char *cur)
+{
+	cprintf("%04x: I am '%s'\n", sys_getenvid(), cur);
+
+	forkchild(cur, '0');
+	forkchild(cur, '1');
+}
+
+void
+umain(int argc, char **argv)
+{
+	forktree("");
+}

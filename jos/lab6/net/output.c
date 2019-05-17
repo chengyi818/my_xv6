4657e02dcaaf87d6f1b7195fa75f19e8969cee6d
chengyi818 <chengyi818@foxmail.com>
2019-05-17 21:46:25 +0800

    Develop | Finish Lab6 Exercise9

--- jos/lab6/net/output.c
+++ jos/lab6/net/output.c
@@ -10,4 +10,21 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+
+
+	int perm;
+	uint32_t req;
+	envid_t who;
+	while(1) {
+		req = ipc_recv(&who, &nsipcbuf, &perm);
+		/* cprintf("%x got %d from %x\n", sys_getenvid(), req, who); */
+		if(req != NSREQ_OUTPUT) {
+			cprintf("not a nsreq output\n");
+			continue;
+		}
+
+		while(sys_pkt_send(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len) < 0) {
+			sys_yield();
+		}
+	}
 }

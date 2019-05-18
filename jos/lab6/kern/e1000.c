27fe1171e421127b8a80b20179232292fb31eca1
chengyi818 <chengyi818@foxmail.com>
2019-05-18 22:31:32 +0800

    Develop | Finish Lab6 Exercise10

--- jos/lab6/kern/e1000.c
+++ jos/lab6/kern/e1000.c
@@ -14,6 +14,11 @@ struct e1000_tdt *tdt;
 struct e1000_tx_desc tx_desc_array[TXDESCS];
 char tx_buffer_array[TXDESCS][TX_PKT_SIZE];
 
+struct e1000_rdh *rdh;
+struct e1000_rdt *rdt;
+struct e1000_rx_desc rx_desc_array[RXDESCS];
+char rx_buffer_array[RXDESCS][RX_PKT_SIZE];
+
 int e1000_attchfn(struct pci_func *pcif) {
 	pci_func_enable(pcif);
 	DEBUG("base[0]: 0x%x, size[0]: %d\n",
@@ -26,6 +31,7 @@ int e1000_attchfn(struct pci_func *pcif) {
 	DEBUG("status reg: 0x%x", *status_reg);
 
 	e1000_transmit_init();
+	e1000_receive_init();
 
 	/*
 	 * transmit test
@@ -87,6 +93,36 @@ e1000_transmit(void *data, size_t len)
 	uint32_t next = (current + 1) % TXDESCS;
 	tdt->tdt = next;
 	return 0;
+}
+
+static void
+e1000_receive_init()
+{
+	uint32_t *rdbal = (uint32_t *)E1000REG(E1000_RDBAL);
+	uint32_t *rdbah = (uint32_t *)E1000REG(E1000_RDBAH);
+	*rdbal = PADDR(rx_desc_array);
+	*rdbah = 0;
+
+	int i;
+	for (i = 0; i < RXDESCS; i++) {
+		rx_desc_array[i].addr = PADDR(rx_buffer_array[i]);
+	}
+
+	struct e1000_rdlen *rdlen = (struct e1000_rdlen *)E1000REG(E1000_RDLEN);
+	rdlen->len = RXDESCS;
+
+	rdh = (struct e1000_rdh *)E1000REG(E1000_RDH);
+	rdt = (struct e1000_rdt *)E1000REG(E1000_RDT);
+	rdh->rdh = 0;
+	rdt->rdt = RXDESCS-1;
+
+	uint32_t *rctl = (uint32_t *)E1000REG(E1000_RCTL);
+	*rctl = E1000_RCTL_EN | E1000_RCTL_BAM | E1000_RCTL_SECRC;
+
+	// hardcode mac address
+	uint32_t *ra = (uint32_t *)E1000REG(E1000_RA);
+	ra[0] = 0x12005452;
+	ra[1] = 0x5634 | E1000_RAH_AV;
 }
 
 // LAB 6: Your driver code here
